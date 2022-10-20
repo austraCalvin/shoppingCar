6fff5174eeebb96f74a904931bc338b0c9a7ef01
austraCalvin <alexadrerivero16@gmail.com>
2022-10-20 11:41:17 -0400

    correction navigatorOnLine conditions

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,26 +1,30 @@
 const shoppinCarCacheName = ("shoppinCarCache");
-navigator && 
-navigator.serviceWorker && 
-navigator.serviceWorker.register("serviceWorker.js").catch((error) => {
+navigator &&
+    navigator.serviceWorker &&
+    navigator.serviceWorker.register("serviceWorker.js").catch((error) => {
 
-    console.log("error message at register serviceWorker file");
-    console.log(error);
+        console.log("error message at register serviceWorker file");
+        console.log(error);
 
-}).then((response) => {
+    }).then((response) => {
 
-    console.log("response message at register serviceWorker file");
-    console.log(response);
+        console.log("response message at register serviceWorker file");
+        console.log(response);
 
-});
+    });
 async function serviceWorkerInstall(e) {
 
     console.log("response from serviceWorkerInstall event!");
     console.log(e);
 
-    const cache = (await self.caches.open(shoppinCarCacheName));
-    const addCache = cache.addAll(["index.html", "style.css", "script.js", "img/apple.png", "img/bread.png", "img/meat.png", "img/melon.png", "img/onion.png", "img/orange.png"]);
+    if (navigator.onLine) {
 
-    e.waitUntil(addCache);
+        const cache = (await self.caches.open(shoppinCarCacheName));
+        const addCache = cache.addAll(["index.html", "style.css", "script.js", "img/apple.png", "img/bread.png", "img/meat.png", "img/melon.png", "img/onion.png", "img/orange.png"]);
+
+        e.waitUntil(addCache);
+
+    };
 
 };
 function serviceWorkerActivate(e) {
@@ -36,29 +40,33 @@ async function serviceWorkerFetch(e) {
     console.log(e.request);
     console.log(e.request.url);
 
-    const cache = (await self.caches.open(shoppinCarCacheName));
-    const response = (async function(){
+    if (!navigator.onLine) {
 
-        const resp_1 = await cache.match(e.request);
+        const cache = (await self.caches.open(shoppinCarCacheName));
+        const response = (async function () {
 
-        if(resp_1){
+            const resp_1 = await cache.match(e.request);
 
-            console.log(resp_1);
-            return resp_1
+            if (resp_1) {
 
-        }else{
+                console.log(resp_1);
+                return resp_1
 
-            console.log(resp_1);
-            const resp_2 = await cache.match("index.html");
-            return resp_2;
+            } else {
 
-        };
+                console.log(resp_1);
+                const resp_2 = await cache.match("index.html");
+                return resp_2;
 
-    })();
+            };
 
-    console.log(response);
-    e.respondWith(response);
+        })();
 
+        console.log(response);
+        e.respondWith(response);
+
+    };
+    
 };
 self.addEventListener("install", serviceWorkerInstall);
 self.addEventListener("activate", serviceWorkerActivate);
